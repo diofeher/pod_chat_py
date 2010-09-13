ff25e3aa4d6bca5d53fa95c021d11ff5f6da576f
Diógenes <diofeher@gmail.com>
2010-09-13 12:23:54 -0300

    adding support to multi-thread environment

--- client.py
+++ client.py
@@ -9,8 +9,33 @@
     @license: see LICENSE.
 """
 from socket import socket, AF_INET, SOCK_STREAM
+from threading import Thread
 from Tkinter import Tk, Button, Text, LEFT, TOP, Entry
+import Queue
 import settings
+
+class ThreadSafeText(Text):
+    def __init__(self, master, **options):
+        Text.__init__(self, master, **options)
+        self.queue = Queue.Queue()
+        self.update_me()
+    def write(self, line):
+        self.queue.put(line)
+    def clear(self):
+        self.queue.put(None)
+    def update_me(self):
+        try:
+            while 1:
+                line = self.queue.get_nowait()
+                if line is None:
+                    self.delete(1.0, END)
+                else:
+                    self.insert(END, str(line))
+                self.see(END)
+                self.update_idletasks()
+        except Queue.Empty:
+            pass
+        self.after(100, self.update_me)
 
 class Client(object):
     """Client"""
@@ -24,9 +49,11 @@ class Client(object):
         # Connecting socket
         self.socket = socket(AF_INET, SOCK_STREAM)
         self.socket.connect((host, port))
+        thread = Thread(target=self.receiving)
+        thread.start()
         
         # TODO: Mount widget
-        self.text = Text()
+        self.text = ThreadSafeText()
         self.text.pack(side=TOP)
         self.send = Button(text="Send a message", command=self.send_message)
         self.send.pack(side=LEFT)
@@ -36,7 +63,6 @@ class Client(object):
     def receiving(self):
         data = self.socket.recv(1024)
         self.text.insert('end', data)
-        self.disconnect()
     
     def send_message(self):
         msg = self.input.get()
@@ -47,6 +73,5 @@ class Client(object):
         
 if __name__=="__main__":
     client = Client(settings.HOST, settings.PORT)
-    #client.receiving()
     root = Tk()
     root.mainloop()
